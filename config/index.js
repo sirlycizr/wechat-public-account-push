706fd27f6f77d6d4bba71392481dd11dadba2498
sirlycizr <114079916+sirlycizr@users.noreply.github.com>
2022-09-21 21:32:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wxf15f2956c70fc34f",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "56e2695f4a92e0b741253c79d8bbc1f3",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -62,9 +62,9 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oI36d5zGn0SD_CnKc6ANnlALpPJU",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: " 	ac32mOhxsrCi5SrcPmHvfZkB8k28cw3tB29l_FP9Qz4",
       // 所在省份
       province: "广东",
       // 所在城市
